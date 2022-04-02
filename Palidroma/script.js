1e1e7c47c039f93f805afd6f4b33942db45ab858
DomenicoLerose85 <domenicolerose85@libero.it>
2022-04-02 18:49:38 +0200

    end_of_excercise

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,27 +1,25 @@
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// // Chiedere all’utente di inserire una parola
+// // Creare una funzione per capire se la parola inserita è palindroma
 
-// variabili
+// // chiedi all'utente di inserire una parola
 
-let parola;
-let parolaSplit;
-let parolaInvertita;
+let parolaReverse;
 
-// chiedo all'utente di inserire una parola
-parola = prompt('Inserisci una parola');
 
-// inversione della parola inserita per verificare se la stessa è 
-// una parola Plidroma
 
-parolaSplit = parola.split('');
-parolaContrario = parolaSplit.reverse();
-parolaInvertita = parolaContrario.join('');
-console.log(parolaSplit);
+let parola = prompt("inserire una parola");
 
-// controllo parola invertita con parola inserita dall'utente
 
-if (parolaInvertita == parola ){
-    document.getElementById("word").innerHTML = "La parola" + ' ' + parola + ' ' + "è una parola Palindroma"
-} else {
-    document.getElementById("word").innerHTML = "La parola" + ' ' + parola + ' ' + "non è una parola Palindroma"
-}
+function InvertiParola(parola){
+       var parolaReverse = "";
+       for( i = parola.lenght - 1; i >= 0; i-- ) {
+       parolaReverse += parola[i];
+    }
+      return parolaReverse;
+    }
+
+      if (parola == InvertiParola(parola)) {
+        console.log("è palindroma");
+    } else if (parola != InvertiParola(parola)) {
+     console.log("non è palindroma");  
+ } 
